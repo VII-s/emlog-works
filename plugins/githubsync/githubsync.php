5a6f16b39a034ee61ceb777ff1f1d550c4c747a0
Mike Tang <mike@mikecoder.net>
2015-07-01 11:11:03 +0800

    github 同步插件1.0

--- plugins/githubsync/githubsync.php
+++ plugins/githubsync/githubsync.php
@@ -1,39 +1,22 @@
 <?php
 /*
-Plugin Name: Advance Markdown Parser
+Plugin Name: GITHUB 文章同步
 Version: 1.0
 Plugin URL: http://mikecoder.net
-Description: Markdown!
+Description: 一个URL,即可完成文章同步。
 Author: Mike
-Author Email: mikecoder13@gmail.com
+Author Email: mike@mikecoder.net
 Author URL: http://mikecoder.net
 */
 !defined('EMLOG_ROOT') && exit('access deined!');
-function dd($value) {
-    var_dump($value);
-    die();
-}
 
 function githubsync(){
     $active_plugins = Option::get('active_plugins');
 }
 
-function githubsync_savelog($id){
-    header("Content-Type:text/html;charset=utf-8");
-    global $logData, $Log_Model;
-    include(EMLOG_ROOT.'/content/plugins/githubsync/lib/parsedown.php');
-    $Parsedown = new Parsedown();
-    $logData['content'] = $Parsedown->text($logData['content']);
-    $logData['content'] = str_replace('<pre><code>', '<pre class="brush:php; toolbar:true; auto-links: true;">', $logData['content']);
-    $logData['content'] = str_replace('<code>', '<pre class="brush:php; toolbar:true; auto-links: true;">', $logData['content']);
-    $logData['content'] = str_replace('</code></pre>', '</pre>', $logData['content']);
-    $logData['content'] = str_replace('</code>', '</pre>', $logData['content']);
-    $Log_Model->updateLog($logData, $id);
-}
-
 function githubsync_editurl() {
     echo '<script type="text/javascript" src="'.BLOG_URL.'content/plugins/githubsync/js/check.js"></script>';
-    echo '<a id="githubdoc" href="javascript:getGithubDocByUrl();" class="thickbox">填写GITHUB连接后点这:</a>&nbsp<input id="githuburl" style="width:600px"></input><br>';
+    echo '&nbsp&nbsp<a id="githubdoc" href="javascript:getGithubDocByUrl();" class="thickbox">填写GITHUB URL后点这:</a>&nbsp<input id="githuburl" style="width:550px"></input><br>';
 }
 addAction('adm_writelog_head', 'githubsync_editurl');
 
