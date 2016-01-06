53ea47d62c2f1a3ac26b2587ff09b0e2804de554
Mike Tang <mike@mikecoder.net>
2016-01-06 22:47:06 +0800

    add pluginseditor plugin

--- plugins/githubsync/githubsync_show.php
+++ plugins/githubsync/githubsync_show.php
@@ -1,5 +1,35 @@
 <?php
-!defined('EMLOG_ROOT') && exit('access deined!');
+
+function getPageContent($url, $timeout = 60, $header = array()){  
+    if (!function_exists('curl_init')) {  
+        throw new Exception('server not install curl');  
+    }  
+    $ch = curl_init();  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+    curl_setopt($ch, CURLOPT_HEADER, true);  
+    curl_setopt($ch, CURLOPT_URL, $url);  
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);  
+    if (!empty($header)) {  
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);  
+    }  
+    $data = curl_exec($ch);  
+    list($header, $data) = explode("\r\n\r\n", $data);  
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);  
+    if ($http_code == 301 || $http_code == 302) {  
+        $matches = array();  
+        preg_match('/Location:(.*?)\n/', $header, $matches);  
+        $url = trim(array_pop($matches));  
+        curl_setopt($ch, CURLOPT_URL, $url);  
+        curl_setopt($ch, CURLOPT_HEADER, false);  
+        $data = curl_exec($ch);  
+    }  
+
+    if ($data == false) {  
+        curl_close($ch);  
+    }  
+    @curl_close($ch);  
+    return $data;  
+}
 
 function dd($value) {
     d($value);
@@ -20,6 +50,7 @@ function json_return($value) {
 }
 
 $URL = base64_decode($_GET['github-url']);
+d($URL);
 
 if (!strstr($URL, 'github')) {
     echo 'NOT GITHUB URL';
@@ -27,14 +58,27 @@ if (!strstr($URL, 'github')) {
 }
 
 preg_match('/github.com\/(.*?)\/(.*?)\//', $URL, $DOC_INFO);
+d($DOC_INFO);
 // URL_IMAGE : img src="/MikeCoder/MyStudy/raw/master/MyBlogs/images/2014-04-14-1.png" alt
 // URL_REAL : raw.githububusercontent.com/MikeCoder/MyStudy$url = "http://www.php100.com/logo.gif";
-$CONTENT = file_get_contents($URL);
+$CONTENT = getPageContent($URL);
+d($CONTENT);
+
 $CONTENT = strstr($CONTENT, '"mainContentOfPage">');
+d($CONTENT);
+
 $CONTENT = substr($CONTENT, strlen('"mainContentOfPage">'));
+d($CONTENT);
+
 $CONTENT = str_replace(strstr($CONTENT, '</article'), '', $CONTENT);
+d($CONTENT);
+
 $CONTENT = str_replace('\n', '',$CONTENT);
+d($CONTENT);
+
 $CONTENT = str_replace('\r\n', '',$CONTENT);
+
+d($CONTENT);
 
 $IMAGE_URL_OLD = '/img src=(.*?raw)\//';
 $IMAGE_URL_NEW = 'img src="https://raw.githubusercontent.com/'.$DOC_INFO[1].'/'.$DOC_INFO[2].'/';
@@ -42,6 +86,7 @@ $CONTENT = preg_replace($IMAGE_URL_OLD, $IMAGE_URL_NEW, $CONTENT);
 $LINK_URL_OLD = '/a href=(.*?blob)\//';
 $LINK_URL_NEW = 'a href="https://raw.githubusercontent.com/'.$DOC_INFO[1].'/'.$DOC_INFO[2].'/';
 $CONTENT = preg_replace($LINK_URL_OLD, $LINK_URL_NEW, $CONTENT);
+d($CONTENT);
 
 $CONTENT = $CONTENT.'<END>';
 $DOC_TITLE_REGEX = '/<h\d+[\s\S]*?a>([\s\S]*?)<\/h\d+>([\s\S]*)<END>/';
